6c18da31c570c024f0c1accc70314dad3219f704
Monish Gupta <monish.gupta1@gmail.com>
2017-02-08 07:44:22 +0530

    Fixed the maxAge number.

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist'), {
-  maxAge: 86400
+  maxAge: 86400000
 }));
 
 app.use('/', index);
